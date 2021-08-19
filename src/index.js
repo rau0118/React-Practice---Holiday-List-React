894696b39755a19a64b0ba54e3fb443ca7850832
Raushan kumar <69362996+rau0118@users.noreply.github.com>
2021-08-19 10:19:03 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function generateCodeFromObject(obj){
     //return a code generated string
    }
    
-module.exports=generateCodeFromObject;
+//module.exports=generateCodeFromObject;
 
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+ ReactDOM.render(<App />, document.getElementById("root"));
